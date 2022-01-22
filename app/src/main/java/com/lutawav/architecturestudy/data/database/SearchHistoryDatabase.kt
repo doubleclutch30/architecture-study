8de6220c647384862cbaef497e17870d3953c39b
doubleclutch30 <cometruth30@gmail.com>
2022-01-22 16:33:58 +0900

    HW-5 (Room) SearchHistoryDatabase 생성

---
+++ app/src/main/java/com/lutawav/architecturestudy/data/database/SearchHistoryDatabase.kt
@@ -0,0 +1,43 @@
+package com.lutawav.architecturestudy.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.lutawav.architecturestudy.data.database.dao.BlogDao
+import com.lutawav.architecturestudy.data.database.dao.ImageDao
+import com.lutawav.architecturestudy.data.database.dao.MovieDao
+import com.lutawav.architecturestudy.data.database.entity.BlogEntity
+import com.lutawav.architecturestudy.data.database.entity.ImageEntity
+import com.lutawav.architecturestudy.data.database.entity.MovieEntity
+
+@Database(
+    entities = [MovieEntity::class, BlogEntity::class, ImageEntity::class],
+    version = 1
+)
+abstract class SearchHistoryDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+    abstract fun blogDao(): BlogDao
+
+    abstract fun imageDao(): ImageDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SearchHistoryDatabase? = null
+
+        fun getInstance(context: Context): SearchHistoryDatabase =
+            INSTANCE ?: synchronized(this) {
+                buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                SearchHistoryDatabase::class.java,
+                "search_history.db"
+            )
+                .build()
+    }
+}
