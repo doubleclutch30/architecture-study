570075ee4792b6f089ac78f81e0323642c6eb5dc
doubleclutch30 <cometruth30@gmail.com>
2022-02-04 18:36:59 +0900

    HW-7 (AAC-LiveData) BaseViewModel에 viewModel, LiveData 적용

--- app/src/main/java/com/lutawav/architecturestudy/ui/BaseViewModel.kt
+++ app/src/main/java/com/lutawav/architecturestudy/ui/BaseViewModel.kt
@@ -3,23 +3,26 @@ package com.lutawav.architecturestudy.ui
 import android.util.Log
 import android.view.View
 import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.lutawav.architecturestudy.data.repository.NaverSearchRepository
 import com.lutawav.architecturestudy.util.hideKeyboard
+import com.lutawav.architecturestudy.util.then
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel<T>(
     protected open val repository: NaverSearchRepository
-) {
-    val viewType = ObservableField(ViewType.VIEW_SEARCH_BEFORE)
-    val errorMsg = ObservableField<String?>()
-    val keyword = ObservableField<String?>()
-    val invalidKeyword = ObservableField<Boolean>()
+) : ViewModel() {
+    val errorMsg = MutableLiveData<String>()
+    val keyword = MutableLiveData<String>()
+    val invalidKeyword = MutableLiveData<Boolean>()
     protected val compositeDisposable = CompositeDisposable()
 
     private val debounceTime: Long = 600L
     private var lastClickTime: Long = 0
 
-    abstract val data: ObservableField<List<T>>
+    abstract val _data: MutableLiveData<List<T>>
 
     abstract fun search(keyword: String)
 
@@ -29,24 +32,22 @@ abstract class BaseViewModel<T>(
         if (System.currentTimeMillis() - lastClickTime < debounceTime) {
             return
         }
-        Log.d("BaseViewModel", "keyword: $keyword")
-        if (keyword.isBlank()) {
-            invalidKeyword.set(true)
-        } else {
-            invalidKeyword.set(false)
+
+        invalidKeyword.value = keyword.isBlank()
+        keyword.isNotBlank().then {
             v.hideKeyboard()
             search(keyword)
         }
     }
 
-    fun onCleared() {
+    override fun onCleared() {
+        super.onCleared()
         compositeDisposable.clear()
     }
 }
 
 enum class ViewType(type: Int) {
-    VIEW_SEARCH_BEFORE(0),
-    VIEW_SEARCH_SUCCESS(1),
-    VIEW_SEARCH_NO_RESULT(2),
+    VIEW_SEARCH_RESULT(0),
+    VIEW_SEARCH_NO_RESULT(1),
     ;
 }
