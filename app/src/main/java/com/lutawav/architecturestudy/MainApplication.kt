1c988e11a784075846822b892c6b11586bac716b
doubleclutch30 <cometruth30@gmail.com>
2022-02-09 18:52:11 +0900

    HW-8 (KOIN) MainApplication Koin 초기설정

--- app/src/main/java/com/lutawav/architecturestudy/MainApplication.kt
+++ app/src/main/java/com/lutawav/architecturestudy/MainApplication.kt
@@ -1,14 +1,31 @@
 package com.lutawav.architecturestudy
 
 import android.app.Application
+import com.lutawav.architecturestudy.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApplication : Application() {
-
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        startKoin {
+            androidLogger(
+                if (BuildConfig.DEBUG) {
+                    Level.DEBUG
+                } else {
+                    Level.NONE
+                }
+            )
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
     }
+
 
     companion object {
         lateinit var instance: MainApplication
