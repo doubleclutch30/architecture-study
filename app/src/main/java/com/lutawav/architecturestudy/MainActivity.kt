98ec862aa393fa5e318ff7efc5d30991bf203615
doubleclutch30 <cometruth30@gmail.com>
2022-02-09 18:05:13 +0900

    HW-7 (AAC-LiveData) MainActivity ViewPager2 적용

--- app/src/main/java/com/lutawav/architecturestudy/MainActivity.kt
+++ app/src/main/java/com/lutawav/architecturestudy/MainActivity.kt
@@ -1,43 +1,76 @@
 package com.lutawav.architecturestudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.lutawav.architecturestudy.databinding.ActivityMainBinding
 import com.lutawav.architecturestudy.ui.blog.BlogFragment
 import com.lutawav.architecturestudy.ui.image.ImageFragment
 import com.lutawav.architecturestudy.ui.movie.MovieFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val movieFragment = MovieFragment()
-        val blogFragment = BlogFragment()
-        val imageFragment = ImageFragment()
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.movie -> replaceFragment(movieFragment)
-                R.id.blog -> replaceFragment(blogFragment)
-                R.id.image -> replaceFragment(imageFragment)
-            }
-            true
+        with(binding) {
+            viewPager.adapter = PagerAdapter(supportFragmentManager, lifecycle)
+            viewPager.registerOnPageChangeCallback(PageChangeCallback())
+            bottomNavigation.setOnItemSelectedListener { navigationSelected(it) }
         }
-
-        replaceFragment(movieFragment)
-
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .apply {
-                replace(R.id.fragmentContainer, fragment)
-                commit()
+    private fun navigationSelected(item: MenuItem): Boolean {
+        val checked = item.setChecked(true)
+        when (checked.itemId) {
+            R.id.movie -> {
+                binding.viewPager.currentItem = 0
+                return true
             }
+            R.id.image -> {
+                binding.viewPager.currentItem = 1
+                return true
+            }
+            R.id.blog -> {
+                binding.viewPager.currentItem = 2
+                return true
+            }
+        }
+        return false
+    }
+
+    private inner class PagerAdapter(
+        fm: FragmentManager, lc: Lifecycle
+    ) : FragmentStateAdapter(fm, lc) {
+
+        private val fragments = arrayListOf<Fragment>(
+            MovieFragment(), ImageFragment(), BlogFragment()
+        )
+
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment =
+            fragments[position]
+    }
+
+    private inner class PageChangeCallback : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            binding.bottomNavigation.selectedItemId = when (position) {
+                0 -> R.id.movie
+                1 -> R.id.image
+                2 -> R.id.blog
+                else -> error("no such position: $position")
+            }
+        }
     }
 }
